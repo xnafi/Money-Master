deec3d5804f9a258fce0579be00f19dd8f0c81f7
Forhad Khan <fk.nafi11@gmail.com>
2022-08-20 21:07:45 +0600

    test final

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function getValueFromInput(id) {
+    const inputField = document.getElementById(id);
+    const inputFieldString = inputField.value;
+    const inputFieldNumber = parseFloat(inputFieldString)
+    return inputFieldNumber
+}
+
+function getValueFromText(id, value) {
+    const textField = document.getElementById(id);
+    const textFieldString = textField.innerText;
+    const textFieldNumber = parseFloat(textFieldString)
+    textField.innerText = value
+    return textFieldNumber;
+
+}
+function getValueFromTextNum(id) {
+    const textField = document.getElementById(id);
+    const textFieldString = textField.innerText;
+    const textFieldNumber = parseFloat(textFieldString)
+    return textFieldNumber;
+
+}
+
+
+document.getElementById('calculate').addEventListener('click', function () {
+    const income = getValueFromInput('income');
+    const food = getValueFromInput('food');
+    const rent = getValueFromInput('rent');
+    const clothes = getValueFromInput('cloth');
+    let totalExpenses = food + rent + clothes;
+    let totalBlance = income - totalExpenses;
+    getValueFromText('total-balance', totalBlance)
+    getValueFromText("expense", totalExpenses)
+
+})
+
+document.getElementById('saving-amount').addEventListener('click', function (totalBlance) {
+    let saving = getValueFromInput('save')
+    let totalBalance = getValueFromTextNum('total-balance')
+    let balance = totalBalance * saving / 100;
+    getValueFromText('saving', balance)
+    const remain = totalBalance - balance
+    getValueFromText('total', remain);
+
+
+})
