8dec825df9c4e780a4a0e974a4e47e942b14218d
Forhad Khan <fk.nafi11@gmail.com>
2022-08-20 21:49:31 +0600

    validation complete

--- js/app.js
+++ js/app.js
@@ -27,8 +27,18 @@ document.getElementById('calculate').addEventListener('click', function () {
     const food = getValueFromInput('food');
     const rent = getValueFromInput('rent');
     const clothes = getValueFromInput('cloth');
+    const nan = income + food + rent + clothes
+    if (isNaN(nan)) {
+        alert('empty field')
+        return 0;
+    }
+
     let totalExpenses = food + rent + clothes;
     let totalBlance = income - totalExpenses;
+    if (totalExpenses > totalBlance) {
+        alert('your expenses greater than your income');
+        return;
+    }
     getValueFromText('total-balance', totalBlance)
     getValueFromText("expense", totalExpenses)
 
@@ -36,11 +46,16 @@ document.getElementById('calculate').addEventListener('click', function () {
 
 document.getElementById('saving-amount').addEventListener('click', function (totalBlance) {
     let saving = getValueFromInput('save')
+    if (isNaN(saving)) {
+        alert('empty field')
+        return 0;
+    }
     let totalBalance = getValueFromTextNum('total-balance')
     let balance = totalBalance * saving / 100;
     getValueFromText('saving', balance)
     const remain = totalBalance - balance
     getValueFromText('total', remain);
+   
 
 
 })
